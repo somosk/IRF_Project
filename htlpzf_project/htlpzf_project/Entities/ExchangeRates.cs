9251a92dd9a4edfec048d92b81fca0802250b273
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-12-10 21:57:36 +0100

    classes

---
+++ htlpzf_project/htlpzf_project/Entities/ExchangeRates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htlpzf_project.Entities
+{
+    public class ExchangeRates
+    {
+        public int year { get; set; }
+        public String Country { get; set; }
+        public double rate { get; set; }
+    }
+}
--- htlpzf_project/htlpzf_project/Form1.cs
+++ htlpzf_project/htlpzf_project/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace htlpzf_project
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +22,19 @@ namespace htlpzf_project
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OpenFileDialog opfd = new OpenFileDialog();
+            if (opfd.ShowDialog()==DialogResult.OK)
+            {
+                string csvpath = opfd.FileName;
+                using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
 
+                    }
+                } ;
+            }
+            
         }
     }
 }
